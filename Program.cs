6c4bb7b8d356b098ef4192c2940d412a37f1e1ac
Lucas Lima <luycaslima@gmail.com>
2023-01-06 13:26:32 +0100

    feat (implementaçao Smartphones)

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia:");
+Smartphone nokia = new Nokia(numero: "123467", modelo: "Modelo 5200", imei: "22222222222", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.Write("\n");
+
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone(numero: "54684970", modelo: "Pro", imei: "33333333333", memoria: 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Youtube");
